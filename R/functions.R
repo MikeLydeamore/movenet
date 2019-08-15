92dbee82f7e1c677154f5d2097a5087b3da7ca8b
Michael Lydeamore <michael.lydeamore@monash.edu>
2019-08-15 01:38:38 +0000

    Add distribution calculating functions for simulating synthetic datasets

--- R/functions.R
+++ R/functions.R
@@ -84,3 +84,145 @@ buildNetwork <- function(df, id, node.column, time.column, nodes.list,
 
   return(x_ij)
 }
+
+
+#' Calculates stay lengths for individuals in a given node.
+#'
+#' @param df Data frame of movement information. Must have a very specific structure, see \code{\link{Details}}
+#' @param id Column containing individual identifier (unquoted)
+#' @param node.column Column containing the node of the individual (unquoted)
+#' @param time.column Column containing the time differences of the individual (unquoted).
+#' @param order Whether the resulting \code{tibble} should be ordered by mean stay (useful for plotting)
+#'
+#' @details
+#' The structure of \code{df} is very specific. For a given individual, each stay will have 2 rows (in order):
+#' the time of admission, and the time of discharge. The node will be the same for  these 2 rows.
+#' The \code{time.column} will then have either the stay length at a given node (even rows), or the time to
+#' next admission (odd rows). The first row of this column is expected to be NA.
+#'
+#' The frame should be ordered by \code{id}, and then by the time (either admission or discharge). Sorting by \code{id}
+#' is not required, but does make the frame nicer to look at.
+#'
+#' @return Tibble containing nodes and the time spent in each node. Note that there will be one row per visit (*not* per ward).
+calcStayLengths <- function(df, id, node.column, time.column, order=T) {
+  id <- enquo(id)
+  node.column <- enquo(node.column)
+  time.column <- enquo(time.column)
+
+  stay_lengths <- df %>% group_by(!!id) %>% slice(seq(2, n(), 2)) %>% ungroup() %>% group_by(!!node.column)
+
+  if (order) {
+    order <- stay_lengths %>% summarise(mean_time=mean(!!time.column, na.rm=T)) %>% arrange(mean_time) %>% select(!!node.column) %>% pull()
+    col <- quo_name(node.column)
+    stay_lengths <- stay_lengths %>% ungroup() %>% mutate((!!col) := factor(!!node.column, levels=order)) %>% group_by(!!node.column)
+  }
+
+  return (stay_lengths)
+}
+
+#' Calculates number of entries into a given node
+#'
+#' @param df Data frame of movement information. Must have a very specific structure, see \code{\link{Details}}
+#' @param id Column containing individual identifier (unquoted)
+#' @param node.column Column containing the node of the individual (unquoted)
+#' @param time.column Column containing the time differences of the individual (unquoted).
+#' @param entry.column Column containing information on whether the row is an entry or exit from the node (unquoted).
+#' @param entry.code String code identifying whether row is an entry or exit from the node (quoted).
+#' @param order Whether the resulting \code{tibble} should be ordered by number of entries (useful for plotting)
+#'
+#' @details
+#' The structure of \code{df} is very specific. For a given individual, each stay will have 2 rows (in order):
+#' the time of admission, and the time of discharge. The node will be the same for  these 2 rows.
+#' The \code{time.column} will then have either the stay length at a given node (even rows), or the time to
+#' next admission (odd rows). The first row of this column is expected to be NA.
+#'
+#' The frame should be ordered by \code{id}, and then by the time (either admission or discharge). Sorting by \code{id}
+#' is not required, but does make the frame nicer to look at.
+#'
+#' @return Tibble containing nodes and the number of entries into each.
+calcNumEntriesIntoNode <- function(df, id, node.column, time.column, entry.column, entry.code, order=T) {
+  id <- enquo(id)
+  node.column <- enquo(node.column)
+  time.column <- enquo(time.column)
+  entry.column <- enquo(entry.column)
+
+  next_event <- df %>% group_by(!!id) %>% filter((!!entry.column) == entry.code) %>% filter(is.na(!!time.column) | (!!time.column) > 0) %>%
+    ungroup() %>% group_by(!!node.column) %>% count()
+
+  if (order) {
+    order <- next_event %>% arrange(n) %>% select(!!node.column) %>% pull()
+    col <- quo_name(node.column)
+    next_event <- next_event %>% ungroup() %>% mutate((!!col) := factor((!!node.column), levels=order)) %>% group_by(!!node.column)
+  }
+
+  return (next_event)
+}
+
+
+#' Calculates number of entries into a given node
+#'
+#' @param df Data frame of movement information. Must have a very specific structure, see \code{\link{Details}}
+#' @param id Column containing individual identifier (unquoted)
+#' @param node.column Column containing the node of the individual (unquoted)
+#' @param time.column Column containing the time differences of the individual (unquoted).
+#' @param entry.column Column containing information on whether the row is an entry or exit from the node (unquoted).
+#' @param entry.code String code identifying whether row is an entry or exit from the node (quoted).
+#' @param order Whether the resulting \code{tibble} should be ordered by mean time (useful for plotting)
+#'
+#' @details
+#' The structure of \code{df} is very specific. For a given individual, each stay will have 2 rows (in order):
+#' the time of admission, and the time of discharge. The node will be the same for  these 2 rows.
+#' The \code{time.column} will then have either the stay length at a given node (even rows), or the time to
+#' next admission (odd rows). The first row of this column is expected to be NA.
+#'
+#' The frame should be ordered by \code{id}, and then by the time (either admission or discharge). Sorting by \code{id}
+#' is not required, but does make the frame nicer to look at.
+#'
+#' @return Tibble containing nodes and the time to next entry into a given node. Note that there is a row *per entry* not per node.
+calcTimeBetweenEntries <- function(df, id, node.column, time.column, entry.column, entry.code, order=T) {
+  id <- enquo(id)
+  node.column <- enquo(node.column)
+  time.column <- enquo(time.column)
+  entry.column <- enquo(entry.column)
+
+  time_between_entries <- df %>% group_by(!!id) %>% filter((!!entry.column) == entry.code) %>% filter(!is.na(!!time.column) & (!!time.column) > 0) %>%
+    ungroup() %>% group_by(!!node.column)
+
+  if (order) {
+    order <- time_between_entries %>% summarise(mean_time=mean(!!time.column, na.rm=T)) %>% arrange(mean_time) %>% select(!!node.column) %>% pull()
+    col <- quo_name(node.column)
+    time_between_entries <- time_between_entries %>% ungroup() %>% mutate((!!col) := factor(!!node.column, levels=order)) %>% group_by(!!node.column)
+  }
+
+  return (time_between_entries)
+}
+
+
+#' Calculates number of direct transfers between nodes
+#'
+#' @param df Data frame of movement information. Must have a very specific structure, see \code{\link{Details}}
+#' @param id Column containing individual identifier (unquoted)
+#' @param node.column Column containing the node of the individual (unquoted)
+#' @param time.column Column containing the time differences of the individual (unquoted).
+#' @param entry.column Column containing information on whether the row is an entry or exit from the node (unquoted).
+#' @param entry.code String code identifying whether row is an entry or exit from the node (quoted).
+#'
+#' @details
+#' The structure of \code{df} is very specific. For a given individual, each stay will have 2 rows (in order):
+#' the time of admission, and the time of discharge. The node will be the same for  these 2 rows.
+#' The \code{time.column} will then have either the stay length at a given node (even rows), or the time to
+#' next admission (odd rows). The first row of this column is expected to be NA.
+#'
+#' The frame should be ordered by \code{id}, and then by the time (either admission or discharge). Sorting by \code{id}
+#' is not required, but does make the frame nicer to look at.
+#'
+#' @return Tibble containing IDs and number of direct transfers between nodes (grouped by number of transfers).
+calcNumberOfTransfers <- function(df, id, node.column, time.column, entry.column, entry.code) {
+  id <- enquo(id)
+  node.column <- enquo(node.column)
+  time.column <- enquo(time.column)
+  entry.column <- enquo(entry.column)
+
+  number_of_changes <- df %>% group_by(!!id) %>% filter((!!entry.column) == entry.code) %%> filter((!!time.column) == 0 | is.na(!!time.column)) %>%
+    count() %>% mutate(n=n-1) %>% ungroup() %>% group_by(n)
+}
